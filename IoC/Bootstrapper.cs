2b0898508d9f267bcb5c743125541b5acae4ea5a
Patrick <leme.patrick.contato@hotmail.com>
2025-02-04 11:37:31 -0300

    Added IoC

---
+++ IoC/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Service.CryptographCredentials;
+using ServiceExternals;
+using ServiceExternals.Interfaces;
+
+namespace CryptographCredentials.IoC
+{
+    public static class Bootstrapper
+    {
+        public static void AddIoC(this IServiceCollection services)
+        {
+            services.AddScoped<ILogHandler, LogHandler>();
+            services.AddScoped<CryptographCredentialsService>();
+        }
+    }
+}
