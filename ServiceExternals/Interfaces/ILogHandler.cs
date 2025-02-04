f03f24843f20a6e856865fbf2504a30d86c51e6a
Patrick <leme.patrick.contato@hotmail.com>
2025-02-04 11:37:57 -0300

    Added ServiceExternals

---
+++ ServiceExternals/Interfaces/ILogHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace ServiceExternals.Interfaces
+{
+    public interface ILogHandler
+    {
+        StringBuilder FileBuilder(string message);
+        Task SaveFileLocallyAsync(string processName, string fileContent);
+    }
+}
---
+++ ServiceExternals/LogHandler.cs
@@ -0,0 +1,49 @@
+﻿using ServiceExternals.Interfaces;
+using System.Text;
+
+namespace ServiceExternals
+{
+    public class LogHandler : ILogHandler
+    {
+
+        #region | Fields |
+        private readonly StringBuilder _stringBuilder;
+        #endregion
+
+        #region | Constructor |
+        public LogHandler()
+        {
+            _stringBuilder = new StringBuilder();
+        }
+        #endregion
+
+        #region | Methods |
+        /// <summary>
+        /// StringBuilder for logging
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public StringBuilder FileBuilder(string message)
+        {
+            return _stringBuilder.AppendLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} | {message}");
+        }
+
+        /// <summary>
+        /// Saves log file locally
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <param name="fileContent"></param>
+        /// <returns></returns>
+        public async Task SaveFileLocallyAsync(string processName, string fileContent)
+        {
+            string fileName = $"{DateTime.Now:yyyMMddTHHmmss}_{processName}";
+
+            string fileFullPath = Path.Combine(Directory.GetCurrentDirectory(), "Log", fileName);
+
+            using var streamWriter = new StreamWriter(fileFullPath, true, Encoding.UTF8, fileContent.Length);
+            await streamWriter.WriteAsync(fileContent);
+            await streamWriter.FlushAsync();
+        }
+        #endregion
+    }
+}
