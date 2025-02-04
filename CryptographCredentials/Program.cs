2c8974e7c07e3cc7454778584e03dc5b4c9832b9
Patrick <leme.patrick.contato@hotmail.com>
2025-02-04 11:39:11 -0300

    Adjusted CryptographCredentials worker

--- CryptographCredentials/Program.cs
+++ CryptographCredentials/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace CryptographCredentials
+﻿using CryptographCredentials.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Service.CryptographCredentials;
+
+namespace CryptographCredentials
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using var host = CreateHostBuilder(args).Build();
+
+            Execute(host.Services);
+        }
+
+        internal static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices((host, services) =>
+                {
+                    services.AddIoC();
+                });
+        }
+
+        internal static void Execute(IServiceProvider serviceProvider)
+        {
+            var service = serviceProvider.GetRequiredService<CryptographCredentialsService>();
+
+            service.Execute();
         }
     }
 }
