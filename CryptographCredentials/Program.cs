49b6130006e8072bffd4e5d81ccbf3628088bae7
Patrick <leme.patrick.contato@hotmail.com>
2025-02-27 10:52:53 -0300

    Source code correction

--- CryptographCredentials/Program.cs
+++ CryptographCredentials/Program.cs
@@ -10,24 +10,19 @@ namespace CryptographCredentials
         static void Main(string[] args)
         {
             using var host = CreateHostBuilder(args).Build();
-
             Execute(host.Services);
         }
 
         internal static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder()
-                .ConfigureServices((host, services) =>
-                {
-                    services.AddIoC();
-                });
+                .ConfigureServices((host, services) => { services.AddIoC(); });
         }
 
         internal static void Execute(IServiceProvider serviceProvider)
         {
             var service = serviceProvider.GetRequiredService<CryptographCredentialsService>();
-
-            service.Execute();
+            service.ExecuteAsync().Wait();
         }
     }
 }
